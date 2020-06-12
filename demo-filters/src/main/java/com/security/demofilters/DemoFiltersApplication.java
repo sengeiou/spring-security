6e9aea1f392540f89808d62ac8d77bb1ee47274a
cailonghao <1719341471@qq.com>
2020-06-12 18:36:20 +0800

    no message

---
+++ demo-filters/src/main/java/com/security/demofilters/DemoFiltersApplication.java
@@ -0,0 +1,13 @@
+package com.security.demofilters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFiltersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoFiltersApplication.class, args);
+    }
+
+}
---
+++ demo-filters/src/test/java/com/security/demofilters/DemoFiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.demofilters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFiltersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
