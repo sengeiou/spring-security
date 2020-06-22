b9087430084f80f3738b447027b88f3b84eae486
cailonghao <1719341471@qq.com>
2020-06-22 13:14:58 +0800

    no message

--- demo-oauth2-server/src/main/java/com/oauth2server/config/Oauth2ServerConfig.java
+++ demo-oauth2-server/src/main/java/com/oauth2server/config/Oauth2ServerConfig.java
@@ -1,31 +1,43 @@
 package com.oauth2server.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 
 /**
  * @author cainiao
  * @program: spring-securiy
  * @description:
  * @create: 2020-06-19 13:38
+ * http://localhost:50000/oauth/authorize?response_type=code&client_id=oauth2_client&scope=all&state=test&redirect_uri=http://example.com
+ * localhost:50000/oauth/token?grant_type=authorization_code&code=qELGB5&redirect_uri=http://example.com
  **/
 @Configuration
 @EnableAuthorizationServer
 public class Oauth2ServerConfig extends AuthorizationServerConfigurerAdapter {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
-                .withClient("javaboy")
+                .withClient("oauth2_client")
                 .secret(new BCryptPasswordEncoder().encode("123"))
                 .resourceIds("res1")
                 .authorizedGrantTypes("authorization_code", "refresh_token")
                 .scopes("all")
-                .redirectUris("http://localhost:8082/index.html");
+                .autoApprove(true)
+                .redirectUris("http://example.com");
     }
 
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
 }
--- demo-oauth2-server/src/main/java/com/oauth2server/config/SecurityConfig.java
+++ demo-oauth2-server/src/main/java/com/oauth2server/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.oauth2server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +20,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
